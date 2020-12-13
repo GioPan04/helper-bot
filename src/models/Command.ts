e0daa03e56a211d72d8a79beae87ac0947ee7e79
Gioele Pannetto <gioele@pannetto.com>
2020-12-13 19:42:55 +0100

    Command improvements

--- src/models/Command.ts
+++ src/models/Command.ts
@@ -1,24 +1,20 @@
 import {Message} from "discord.js";
 
 export class BotCommand {
-    name: String;
-    description: String;
-    usage: String;
-    executor: Function;
-    minArgs: Number;
-    
-    constructor(name: String, description: String, usage: String, executor: Function, minArgs: Number) {
-        this.name = name;
-        this.description = description;
-        this.usage = usage;
-        this.executor = executor;
-        this.minArgs = minArgs ?? 0;
-    }
+
+    constructor(
+        public name: string,
+        public description: string,
+        public usage: string,
+        public executor: Function,
+        public minArgs: number
+    ) {}
 
     execute(msg: Message) {
         let args = msg.content.split(' ').slice(1);
         if(args.length < this.minArgs) return msg.channel.send(this.usage.toString());
         this.executor();
     }
+    
 }
 
