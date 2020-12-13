5590855c4900ecd1251e8a65bb203b9ee9cca5b2
Gioele Pannetto <gioele@pannetto.com>
2020-12-13 16:02:15 +0100

    Help embed

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,5 +1,16 @@
-import {Message} from 'discord.js';
+import Discord, {Message} from 'discord.js';
+require('dotenv').config();
+
+const helpEmbed = new Discord.MessageEmbed()
+    .setColor('#FF0000')
+    .setAuthor("Helper Bot's commands")
+    .setDescription(`Use ${process.env.CMD_PREFIX} as prefix`)
+    .addFields(
+        { name: 'ping', value: 'Ping the bot' },
+        { name: 'help', value: 'Shows this message' }
+    );
 
 export default function (msg: Message) {
     msg.channel.send("Here's a list of all commands avaible:");
+    msg.channel.send(helpEmbed);
 }
